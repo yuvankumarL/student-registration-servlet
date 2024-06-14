e4d864ba2b41c94deeb7cd7072e303c0dcbbf7bd
Yuvankumar <93638740+yuvankumarL@users.noreply.github.com>
2024-06-14 23:34:53 +0530

    Create StudentRegistrationServlet.java

---
+++ StudentRegistrationServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StudentRegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Handle GET requests here
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Handle POST requests here
+    }
+}
